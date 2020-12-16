9df6ab321feee58bef15f2af4c757ae24256d839
lfrank01 <lfrank01@charlottesvilleschools.org>
2020-12-16 09:47:01 -0500

    Created FancyLED class structure to match given main.py file.

---
+++ FancyLED/fancyLED.py
@@ -0,0 +1,19 @@
+import board
+
+
+class FancyLED:
+
+    def __init__(self, led1, led2, led3):
+        pass
+
+    def alternate(self):
+        pass
+
+    def blink(self):
+        pass
+
+    def chase(self):
+        pass
+
+    def sparkle(self):
+        pass
